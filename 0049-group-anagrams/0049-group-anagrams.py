66a83c639f7213e9ccace06d9d6e7a3a55d9af5e
Chinmai SD <chinmaisdinesh@gmail.com>
2026-07-12 15:37:41 +0530

    Time: 23 ms (49.48%), Space: 16 MB (67.97%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+    # BRUTE FORCE time complexity o(n^2)
+        # visited= [False]*len(strs)
+        # ans=[]
+        # for i in range(len(strs)):
+        #     if visited[i]:
+        #         continue
+        #     group=[strs[i]] 
+        #     visited[i]=True
+        #     for j in range(i+1, len(strs)):
+        #         if sorted(strs[i])==sorted(strs[j]):
+        #             group.append(strs[j])
+        #             visited[j]=True
+        #     ans.append(group)
+        # return ans
+
+
+    # hashmap optimal sol time complexity (o(nklogk))
+        hashmap={}
+        for word in strs:
+            key="".join(sorted(word))
+            if key not in hashmap:
+                hashmap[key]=[word]
+            else:
+                hashmap[key].append(word)
+        return hashmap.values()
