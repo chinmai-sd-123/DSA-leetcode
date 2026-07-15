600fe6e881e56ad35fb3730f8cf5b65f91578fc9
Chinmai SD <chinmaisdinesh@gmail.com>
2026-07-15 21:58:22 +0530

    Time: 54 ms (55.14%), Space: 27.1 MB (13.53%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+    # brute force solution with time complexity - o(n^2)
+        # max_len=0
+        # for i in range(len(nums)):
+        #     current=nums[i]
+        #     length=1
+        #     while current+1 in nums:
+        #         current+=1
+        #         length+=1
+        #     max_len=max(max_len, length)
+        # return max_len
+
+    # optimal solution with time complexity - o(n)
+    # bcz heree it does go check every chanin again and again
+        num_set=set(nums)
+        max_len=0
+        for num in num_set:
+            if num-1 not in num_set:
+                current=num
+                length=1
+                while current+1 in num_set:
+                    current+=1
+                    length+=1
+                max_len=max(max_len, length)
+
+        return max_len
