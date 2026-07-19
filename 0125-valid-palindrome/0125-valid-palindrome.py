3584cd628a35952ce7e09c27187b0be03621ef3a
Chinmai SD <chinmaisdinesh@gmail.com>
2026-07-19 17:14:17 +0530

    Time: 42 ms (35%), Space: 12.6 MB (76.99%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,36 +4,19 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-
-    # two pointers with time complexity - o(n) space -o(n)
-        # clean=[]
-        # for ch in  s:
-        #     if ch.isalnum():
-        #         clean.append(ch)
-
-        # clean_s="".join(clean).lower()
-        
-        # left=0
-        # right= len(clean_s)-1
-        # while left<right:
-        #     if clean_s[left]!=clean_s[right]:
-        #         return False
-        #     left+=1
-        #     right-=1
-        # return True
-
-    # two pointers with space o (1)
-        left=0
-        right=len(s)-1
-        while left<right:
-            if not s[left].isalnum():
+        left, right= 0, len(s)-1
+        while left< right:
+            while left < right and not self.isalphaNum(s[left]):
                 left+=1
-            elif not s[right].isalnum():
+            while right > left and not self.isalphaNum(s[right]):
                 right-=1
-            elif  s[left].lower()!=s[right].lower():
+
+            if s[left].lower()!=s[right].lower():
                 return False
-            else:
-                left+=1
-                right-=1
+            left, right = left+1, right -1
         return True
-        
+    
+    def isalphaNum(self, c):
+        return (ord('A') <= ord(c) <=ord('Z') or
+                ord('a') <= ord(c) <=ord('z') or
+                ord('0') <= ord(c) <=ord('9'))
