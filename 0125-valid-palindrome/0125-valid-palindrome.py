76b69417471c8c77bb92975e1c1f8e4cb00df5fd
Chinmai SD <chinmaisdinesh@gmail.com>
2026-07-19 17:15:25 +0530

    Time: 38 ms (35.55%), Space: 12.6 MB (91.77%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -20,3 +20,37 @@ class Solution(object):
         return (ord('A') <= ord(c) <=ord('Z') or
                 ord('a') <= ord(c) <=ord('z') or
                 ord('0') <= ord(c) <=ord('9'))
+
+
+
+         # two pointers with time complexity - o(n) space -o(n)
+        # clean=[]
+        # for ch in  s:
+        #     if ch.isalnum():
+        #         clean.append(ch)
+
+        # clean_s="".join(clean).lower()
+        
+        # left=0
+        # right= len(clean_s)-1
+        # while left<right:
+        #     if clean_s[left]!=clean_s[right]:
+        #         return False
+        #     left+=1
+        #     right-=1
+        # return True
+
+    # two pointers with space o (1)
+        # left=0
+        # right=len(s)-1
+        # while left<right:
+        #     if not s[left].isalnum():
+        #         left+=1
+        #     elif not s[right].isalnum():
+        #         right-=1
+        #     elif  s[left].lower()!=s[right].lower():
+        #         return False
+        #     else:
+        #         left+=1
+        #         right-=1
+        # return True
