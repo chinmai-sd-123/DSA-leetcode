6e696f04d93bf678c191aa64b90a2a91ce9a1083
Chinmai SD <chinmaisdinesh@gmail.com>
2026-08-14 20:30:42 +0530

    Time: 0 ms (100%), Space: 12.2 MB (92.65%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        #find pivot from right
+        pivot=-1
+        for i in range(len(nums)-2,-1,-1):
+            if nums[i]<nums[i+1]:
+                pivot=i
+                break
+
+        if pivot==-1:
+            nums.reverse()
+            return
+        #search for next greater than pivot
+        j=len(nums)-1
+        while nums[j]<=nums[pivot]:
+            j-=1
+        # swap
+        nums[pivot], nums[j]= nums[j], nums[pivot]
+        # reverse suffix of pivot
+        # nums[pivot+1:]=nums[pivot+1:][::-1]
+        
+        l=pivot+1
+        r=len(nums)-1
+        while l<r:
+            nums[l], nums[r]=nums[r],nums[l]
+            l+=1
+            r-=1
+      
